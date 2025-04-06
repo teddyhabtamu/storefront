6ee19cd9250f2eded646abba104c496d0207fff8
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-06 19:52:21 +0300

    django views

---
+++ playground/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+# A view function is a Python function that takes a web request and returns a web response.
+
+def say_hello(request):
+  return HttpResponse("Hello, world!")
