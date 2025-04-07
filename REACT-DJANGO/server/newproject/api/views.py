15804802176797467b1f60b9c5241a663017e9b1
teddyhabtamu <tewodroshabtamu29@gmail.com>
2025-04-07 19:32:40 +0300

    Django REST Framework

--- REACT-DJANGO/server/newproject/api/views.py
+++ REACT-DJANGO/server/newproject/api/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Book
+from .serializer import BookSerializer
 
-# Create your views here.
+# write end points
+
+@api_view(['GET'])
+def get_books(request):
+  books = Book.objects.all()
+  serializedData = BookSerializer(books, many=True).data
+  return Response(serializedData)
+
